d64e5f5488d5531a28413105720040272ed68014
Scile11 <j.potinius@web.de>
2020-02-23 12:09:00 +0100

    Neueste

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,12 @@
 function navscroll(target) {
-    const elem = document.getElementById(target)
-    elem.scrollIntoView({
-        behavior: 'smooth',
-        block: 'center'
-    });
+    const elem = document.getElementById(target);
+    const tar = elem.getBoundingClientRect();
+    window.scrollTo({
+        top: (tar.top + window.pageYOffset) - document.getElementById("header").clientHeight,
+        behavior: 'smooth'
+    })
+}
+
+function toggledark() {
+    document.body.classList.toggle('light');
 }
