7f14280e2f4be9a1a443ec04a815b9a74a9980b1
Scile11 <j.potinius@web.de>
2020-02-23 14:04:16 +0100

    Neue Kommentare

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
 function navscroll(target) {
-    const elem = document.getElementById(target);
-    const tar = elem.getBoundingClientRect();
-    window.scrollTo({
-        top: (tar.top + window.pageYOffset) - document.getElementById("header").clientHeight,
+    const elem = document.getElementById(target);//importiere Ziel Element
+    const tar = elem.getBoundingClientRect();//Speicher position relativ zum viewport
+    //Scrolle fester position
+    window.scrollTo(
+    {//Funktion nimmt objekt mit Zielposition und Verhalten zbs weiches scrolling/Instant scrolling.
+        top: (tar.top + window.pageYOffset) - document.getElementById("header").clientHeight,//Höhe des Elements mit der Id header
         behavior: 'smooth'
-    })
+    }
+    )
 }
 
 function toggledark() {
-    document.body.classList.toggle('light');
+    document.body.classList.toggle('light');//toggle light klasse für Body
 }
